de408a0d0fd8bd57aadc53d63808f102845d8fa4
kimdohyun <85009117+kimrasng@users.noreply.github.com>
2025-11-05 16:49:08 +0900

    11.5

--- REST_Framework/equipment/swequipment/urls.py
+++ REST_Framework/equipment/swequipment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import helloAPI, equipmentAPI
+from .views import helloAPI, equipmentsAPI, equimentAPI
 
 urlpatterns = [
 	path('hello/', helloAPI),
-    path('equipments/', equipmentAPI),
+    path('equipments/', equipmentsAPI),
+    path('equipment/<int:id>/', equimentAPI)
 ]
--- REST_Framework/equipment/swequipment/views.py
+++ REST_Framework/equipment/swequipment/views.py
@@ -1,6 +1,9 @@
+from gc import get_objects
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 from .models import Equipment
 from .serizlizers import EquipmentSerializer
@@ -14,7 +17,7 @@ def helloAPI(request):
     return Response({"msg": "hello"})
 
 @api_view(["GET", "POST"])
-def equipmentAPI(request):
+def equipmentsAPI(request):
     if request.method == "GET":
         equipments = Equipment.objects.all()
         serializer = EquipmentSerializer(equipments, many=True)
@@ -24,4 +27,20 @@ def equipmentAPI(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "DELETE", "PUT"])
+def equimentAPI(request, id):
+    equipment = get_object_or_404(Equipment, eid=id)
+    if request.method == "GET":
+        serializer = EquipmentSerializer(equipment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        equipment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == "PUT":
+        serializer = EquipmentSerializer(equipment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
