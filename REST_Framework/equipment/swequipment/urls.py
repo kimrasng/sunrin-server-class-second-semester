bb5c70a71f810a166e188957927cf6d465924516
kimdohyun <85009117+kimrasng@users.noreply.github.com>
2025-11-06 17:01:23 +0900

    11.6

--- REST_Framework/equipment/swequipment/urls.py
+++ REST_Framework/equipment/swequipment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import helloAPI, equipmentsAPI, equimentAPI
+from .views import helloAPI, equipmentsAPI, equimentAPI, EquimentsAPI, EquimentAPI
 
 urlpatterns = [
 	path('hello/', helloAPI),
     path('equipments/', equipmentsAPI),
-    path('equipment/<int:id>/', equimentAPI)
+    path('equipment/<int:id>/', equimentAPI),
+    path('class/equipments/', EquimentsAPI.as_view()),
+    path('class/equipment/<int:id>/', equimentAPI),
 ]
--- REST_Framework/equipment/swequipment/views.py
+++ REST_Framework/equipment/swequipment/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
 
 from .models import Equipment
 from .serizlizers import EquipmentSerializer
@@ -39,6 +40,41 @@ def equimentAPI(request, id):
         equipment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     elif request.method == "PUT":
+        serializer = EquipmentSerializer(equipment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EquimentsAPI(APIView):
+    def get(selfself, request):
+        equipments = Equipment.objects.all()
+        serializer = EquipmentSerializer(equipments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = EquipmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EquimentAPI(APIView):
+    def get_object(self, id):
+        return get_object_or_404(Equipment, eid=id)
+
+    def get(self, request, id):
+        equipment = self.get_object(id)
+        serializer = EquipmentSerializer(equipment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        equipment = self.get_object(id)
+        equipment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id):
+        equipment = self.get_object(id)
         serializer = EquipmentSerializer(equipment, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
